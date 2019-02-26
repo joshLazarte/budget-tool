d27a20d073db01620151515291f5e7eb2ccaeb5b
Josh <joshualazarte@yahoo.com>
2019-02-25 21:45:01 -0600

    build out ejs template and compile data to template

--- routes/API.js
+++ routes/API.js
@@ -1,21 +1,22 @@
 /* eslint-disable no-console */
 const express = require('express'),
       router  = express.Router(),
-      fs      = require('fs');
+      fs      = require('fs'),
+      ejs     = require('ejs');
 
 
 router.get('/', (req,res) => {
     res.send('API Get Route')
 });  
 
-router.post('/', (req,res) => {        
-    fs.writeFile('./db/data.json', JSON.stringify(req.body), (err) => {
-        if(err) {
-            console.log(err)
-        }
-    });
-    res.send({"data": "received"})
-        
+router.post('/', (req,res) => {    
+    const data = req.body;
+    const compiled = ejs.compile(fs.readFileSync('./views/template.ejs', 'utf8'));
+    const html = compiled({data});
+    console.log(html);
+
+
+    res.send({"data": "received"})        
 }); 
 
 module.exports = router;
