ece544eb0c8179cd8da8217551410b50c568da6b
Josh <joshualazarte@yahoo.com>
2019-02-25 22:07:55 -0600

    working pdf version, stylesheet not applied

--- routes/API.js
+++ routes/API.js
@@ -2,6 +2,7 @@
 const express = require('express'),
       router  = express.Router(),
       fs      = require('fs'),
+      puppeteer = require('puppeteer'),
       ejs     = require('ejs');
 
 
@@ -9,13 +10,29 @@ router.get('/', (req,res) => {
     res.send('API Get Route')
 });  
 
-router.post('/', (req,res) => {    
-    const data = req.body;
-    const compiled = ejs.compile(fs.readFileSync('./views/template.ejs', 'utf8'));
+const compile = function(template, data){
+    const compiled = ejs.compile(fs.readFileSync(template, 'utf8'));
     const html = compiled({data});
-    console.log(html);
+    return html;
+}
 
+router.post('/', async (req,res) => {   
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
+    const content = compile('./views/template.ejs', req.body);
+
+    await page.setContent(content);
+    await page.emulateMedia('screen');
+    await page.pdf({
+        path: 'budget.pdf',
+        format: 'A4',
+        printBackground: true
+    });
+
+    console.log('done');
+    await browser.close();
+    process.exit();
     res.send({"data": "received"})        
 }); 
 
