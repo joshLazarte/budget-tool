6fe6dec2c2e13492e0ec28ff6f7e040219a8e106
Josh <joshualazarte@yahoo.com>
2019-02-28 17:35:06 -0600

    save pdf, no download

--- routes/API.js
+++ routes/API.js
@@ -10,30 +10,38 @@ router.get('/', (req,res) => {
     res.send('API Get Route')
 });  
 
-const compile = function(template, data){
+const compile = function(template, data, css){
     const compiled = ejs.compile(fs.readFileSync(template, 'utf8'));
-    const html = compiled({data});
+    const html = compiled({data, css});
     return html;
+}
+
+const getDataFromFile = function(file) {    
+   const data = fs.readFileSync(file, 'utf8', (err, contents) => {
+            if(err) throw err;
+            return contents;
+        });
+    return data;
 }
 
 router.post('/', async (req,res) => {   
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    const page = await browser.newPage(); 
 
-    const content = compile('./views/template.ejs', req.body);
+    const css = getDataFromFile('./public/css/style.css'); 
+
+    const content = compile('./docs/template.ejs', req.body, css);   
 
     await page.setContent(content);
-    await page.emulateMedia('screen');
+    await page.emulateMedia('screen');    
     await page.pdf({
-        path: 'budget.pdf',
+        path: './docs/budget.pdf',
         format: 'A4',
         printBackground: true
     });
 
     console.log('done');
-    await browser.close();
-    process.exit();
-    res.send({"data": "received"})        
+    await browser.close();           
 }); 
 
 module.exports = router;
