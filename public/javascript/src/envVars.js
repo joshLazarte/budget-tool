0f19f5d2ab579868aa2a2c789c3d3b659cc2e4da
Josh Lazarte <joshualazarte@yahoo.com>
2019-08-05 14:10:44 +0000

    setup gitignore

--- public/javascript/src/envVars.js
+++
@@ -1,6 +0,0 @@
-const envVars = {
-    postRoute: 'https://j3l-webdesign-lazarte.c9users.io/pdf',
-    deleteRoute: 'https://j3l-webdesign-lazarte.c9users.io/pdf/delete'
-};
-
-export default envVars;
