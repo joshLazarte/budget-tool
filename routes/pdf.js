722d94a0d4ab5e69b8e684405195e6a07f8fd2f2
Josh <joshualazarte@yahoo.com>
2019-08-05 21:27:48 -0500

    rename pdf

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,54 +1,56 @@
 /* eslint-disable no-console */
-const express = require('express'),
-      router  = express.Router(),
-      fs      = require('fs'),      
-      pdf     = require('html-pdf'),
-      path    = require('path'),
-      ejs     = require('ejs');
+const express = require("express"),
+  router = express.Router(),
+  fs = require("fs"),
+  pdf = require("html-pdf"),
+  path = require("path"),
+  ejs = require("ejs");
 
 const compile = (template, data, css) => {
-    const compiled = ejs.compile(fs.readFileSync(template, 'utf8'));
-    const html = compiled({data, css});
-    return html;
+  const compiled = ejs.compile(fs.readFileSync(template, "utf8"));
+  const html = compiled({ data, css });
+  return html;
 };
 
-const getDataFromFile = (file) => {    
-   const data = fs.readFileSync(file, 'utf8', (err, contents) => {
-            if(err) throw err;
-            return contents;
-        });
-    return data;
+const getDataFromFile = file => {
+  const data = fs.readFileSync(file, "utf8", (err, contents) => {
+    if (err) throw err;
+    return contents;
+  });
+  return data;
 };
 
-router.post('/', (req, res) => {      
-    const css = getDataFromFile('./public/css/style.css'); 
-    const content = compile('./docs/template.ejs', req.body, css);     
-    const options = {format: 'Letter'};
-    const fileName = path.join('./docs/', new Date().toISOString() + '-budget.pdf');
-    req.session.fileName = fileName;
+router.post("/", (req, res) => {
+  const css = getDataFromFile("./public/css/style.css");
+  const content = compile("./docs/template.ejs", req.body, css);
+  const options = { format: "Letter" };
 
-    pdf.create(content, options).toFile(fileName, (err, pdf) => {
-        if (err) return console.log(err);
-        res.send({"status": "success"}); 
-    }); 
-}); 
+  const fileName = `./docs/${Date.now()}-budget.pdf`;
 
-router.delete('/delete', (req,res) => {
-    const file = req.session.fileName;
-    req.session.fileName = undefined;
-    if(file !== undefined && fs.existsSync(file)) {
-        fs.unlinkSync(file);
-        res.send({"action": "file deleted"});
-    }
+  req.session.fileName = fileName;
+
+  pdf.create(content, options).toFile(fileName, (err, pdf) => {
+    if (err) return console.log(err);
+    res.send({ status: "success" });
+  });
 });
 
-router.get('/download', (req,res) => {
-    const file = req.session.fileName;
-    req.session.fileName = undefined;
-    res.download(file, (err) => {
-        if (err) return console.log(err);
-        fs.unlinkSync(file);
-    });
-});  
-      
+router.delete("/delete", (req, res) => {
+  const file = req.session.fileName;
+  req.session.fileName = undefined;
+  if (file !== undefined && fs.existsSync(file)) {
+    fs.unlinkSync(file);
+    res.send({ action: "file deleted" });
+  }
+});
+
+router.get("/download", (req, res) => {
+  const file = req.session.fileName;
+  req.session.fileName = undefined;
+  res.download(file, err => {
+    if (err) return console.log(err);
+    fs.unlinkSync(file);
+  });
+});
+
 module.exports = router;
