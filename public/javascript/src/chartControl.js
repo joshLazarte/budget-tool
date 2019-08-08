afe4cf44692d06c245b457cc42de8773df443179
Josh Lazarte <joshualazarte@yahoo.com>
2019-08-08 15:39:24 +0000

    show chart in pdf template

--- public/javascript/src/chartControl.js
+++ public/javascript/src/chartControl.js
@@ -5,16 +5,16 @@ const _chartCtrl = {
         chart.data.datasets[0].data = chartData;
         chart.update();
     },
-    myChart:  new Chart(ctx, {
+    myChart: new Chart(ctx, {
         type: 'pie',
         data: {
             labels: [
                 "Surplus",
-                "Bills", 
-                "Debt", 
-                "Groceries", 
-                "Prepared Food", 
-                "Entertainment", 
+                "Bills",
+                "Debt",
+                "Groceries",
+                "Prepared Food",
+                "Entertainment",
                 "Subscriptions",
                 "Miscellaneous"
             ],
@@ -31,16 +31,6 @@ const _chartCtrl = {
                     'rgba(255, 159, 64, 1)',
                     'rgba(255, 159, 255, 1)'
                 ],
-                // borderColor: [
-                //     'rgba(68, 201, 50, 1)',
-                //     'rgba(255,99,132,1)',
-                //     'rgba(54, 162, 235, 1)',
-                //     'rgba(255, 206, 86, 1)',
-                //     'rgba(75, 192, 192, 1)',
-                //     'rgba(153, 102, 255, 1)',
-                //     'rgba(255, 159, 64, 1)',
-                //     'rgba(255, 159, 255, 1)'
-                // ],
                 borderWidth: 0
             }]
         },
